6d01debf208f3afe49aad42583dbf642c8da7e23
Pragy Agarwal <agar.pragy@gmail.com>
2020-06-12 00:22:35 +0530

    Update init.sh

--- utils/init.sh
+++ utils/init.sh
@@ -3,4 +3,5 @@ sudo apt-get --assume-yes install build-essential curl file git
 git clone --bare https://github.com/AgarwalPragy/.cfg.git ~/.cfg
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME reset --hard
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME remote set-url origin git@github.com:AgarwalPragy/.cfg.git
+git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no
 bash ~/utils/install_utils.sh
