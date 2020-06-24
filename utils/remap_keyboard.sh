03527b0a78976bab797b911ae5a9fa3e3c0414ad
Pragy Agarwal <agar.pragy@gmail.com>
2020-06-24 07:23:57 +0530

    move keyboard remapping to standalone script. autostart

---
+++ utils/remap_keyboard.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while true
+do
+	# clear all previous mapppings
+	setxkbmap -option
+	# set new mappings
+	setxkbmap -option caps:backspace
+	setxkbmap -option shift:both_capslock
+	xmodmap -e "clear Lock"
+	xset r rate 220 40
+	sleep 10m
+done
