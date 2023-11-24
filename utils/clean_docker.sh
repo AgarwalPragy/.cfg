53e7c450c9e6c4a0faf56bab20bab82a52c1088c
Pragy Agarwal <agar.pragy@gmail.com>
2023-11-24 21:05:54 +0530

    Update clean_docker.sh

--- utils/clean_docker.sh
+++ utils/clean_docker.sh
@@ -1,1 +1,6 @@
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
 docker system prune -a -f --volumes
+docker volume prune --force
+docker network prune --force
+docker image prune --all --force
